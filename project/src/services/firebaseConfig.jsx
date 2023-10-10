e8003699a37739a384741547026d631151b83347
Andre_Victor <95365578+AndreVsc@users.noreply.github.com>
2023-10-09 22:07:21 -0300

    Teste
    Firebase

---
+++ project/src/services/firebaseConfig.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAADvZS1F-7rEkeLn8Lrq0Z4GRC3xBFVcg",
+  authDomain: "silver-archery-384403.firebaseapp.com",
+  projectId: "silver-archery-384403",
+  storageBucket: "silver-archery-384403.appspot.com",
+  messagingSenderId: "494240725571",
+  appId: "1:494240725571:web:00d2161d5be527dc2172f6",
+  measurementId: "G-NB92G4RX0Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
